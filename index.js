34b9538435a41f096c162746817124f0e97deecd
Wes Bos <wesbos@gmail.com>
2021-12-02 15:30:13 -0500

    its alive

---
+++ index.js
@@ -0,0 +1,23 @@
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkVscode from 'gatsby-remark-vscode';
+import rehypeRaw from 'rehype-raw';
+import remarkToRehype from 'remark-rehype';
+import remarkGfm from 'remark-gfm';
+import rehypeStringify from 'rehype-stringify';
+
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkVscode.remarkPlugin, {
+    theme: 'Cobalt2',
+    extensions: [`theme-cobalt2`],
+  })
+  .use(remarkToRehype, { allowDangerousHtml: true })
+  .use(rehypeRaw)
+  .use(rehypeStringify)
+
+export async function convert(markdown) {
+  const file = await processor.process(markdown)
+  console.log(file.value)
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Markdown to HTML
+
+This package converts Github Flavoured Markdown to HTML with my Cobatl2 VS Code theme for syntax highlighting.
+
+This is really only it's own package because there are so many dependencies when using remark/rehype and I rather pin them all in it's own version.
+
